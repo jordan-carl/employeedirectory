fa822be9d196f1ab5ef5dbfc3e75a36474d7d53b
Bernardo Kuri <github@bkuri.com>
2013-05-26 10:10:31 -0500

    Minor changes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+      var self = this;
+      this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+      });
     },
 
     initialize: function() {
-        var self = this;
-        self.store = new MemoryStore(function () {
-          self.showAlert('Store initialized', 'Info');
-        });
-        $('.search-key').on('keyup', $.proxy(self.findByName, self));
+      var self = this;
+      self.store = new MemoryStore(function () {
+        self.renderHomeView();
+      });
+      $('.search-key').on('keyup', $.proxy(self.findByName, self));
+      self.homeTpl = Handlebars.compile($('#home-tpl').html());
+      self.employeeLiTpl = Handlebars.compile($('#employee-li-tpl').html());
+    },
+
+    renderHomeView: function () {
+      var self = this;
+      $('body').html(self.homeTpl());
+      $('.search-key').on('keyup', $.proxy(self.findByName, self));
     },
 
     showAlert: function (message, title) {
